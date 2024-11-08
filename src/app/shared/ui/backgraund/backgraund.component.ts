7c45c7bcb64a141a50f67ad29bc0b17104fb3cc0
Erick Montes <erickmontes77.jm@gmail.com>
2024-11-08 13:21:25 -0500

    Añadir componente Backgraund
    Se agregó un nuevo componente llamado Backgraund, que incluye archivos HTML, CSS, TypeScript y pruebas. Este componente es autónomo y ha sido diseñado para manejar el fondo, con un estilo de diseño en gradiente y pruebas correspondientes para asegurar su correcto funcionamiento.

---
+++ src/app/shared/ui/backgraund/backgraund.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backgraund',
+  standalone: true,
+  imports: [],
+  templateUrl: './backgraund.component.html',
+  styleUrl: './backgraund.component.css'
+})
+export class BackgraundComponent {
+
+}
