81542ccca1110553c3fcfcdc1e08b401ad702347
Erick Montes <erickmontes77.jm@gmail.com>
2024-11-11 15:42:25 -0500

    Actualizar formulario de registro y diseño de fondo
    Se rediseñó el formulario de registro para incluir más campos y mejorar su organización. Además, se implementó una animación de fondo con elementos decorativos y se añadió efecto de desenfoque al contenedor principal.

--- src/app/forms/register/register.component.ts
+++ src/app/forms/register/register.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+    imports: [
+        NgIf
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
