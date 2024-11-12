1890ca9d51a7b16b78730d0dd900e00866358fec
Erick Montes <erickmontes77.jm@gmail.com>
2024-11-12 16:11:47 -0500

    Implementa componente reusable para entradas de formulario
    Se ha creado un nuevo componente de entrada que incluye validación y mensajería de error en tiempo real. También se actualizó el formulario de registro y login para utilizar este nuevo componente, mejorando así la modularidad y reusabilidad del código.

--- src/app/forms/form-register/form-register.component.ts
+++ src/app/forms/form-register/form-register.component.ts
@@ -1,12 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators} from '@angular/forms';
+import {NgForOf, NgIf} from "@angular/common";
+import {InputComponent} from "../../shared/ui/input/input.component";
+
 
 @Component({
-  selector: 'app-form-register',
+  selector: 'form-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputComponent, NgIf, InputComponent, NgForOf],
   templateUrl: './form-register.component.html',
-  styleUrl: './form-register.component.css'
 })
-export class FormRegisterComponent {
+export class FormRegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  formErrors: string[] = [];
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      nombre: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')
+      ]],
+      cedula: ['', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.pattern('^[0-9]{10}$')
+      ]]
+    });
+
+    console.log('Formulario inicializado:', this.registerForm);
+  }
+
+  onSubmit(): void {
+    console.log('Método onSubmit llamado');
+
+    if (this.registerForm.invalid) {
+      this.formErrors = [];
+      const detailedErrors: { field: string; errors: ValidationErrors | null; }[] = [];
+
+      Object.keys(this.registerForm.controls).forEach(key => {
+        const control = this.registerForm.get(key);
+        const controlErrors = control?.errors;
+        if (control && control.invalid) {
+          detailedErrors.push({
+            field: key,
+            errors: control.errors
+          });
+
+          if (controlErrors) {
+            Object.keys(controlErrors).forEach(keyError => {
+              if (keyError === 'required') {
+                this.formErrors.push(`${key} es obligatorio`);
+              } else if (keyError === 'minlength') {
+                this.formErrors.push(`${key} debe tener al menos ${controlErrors[keyError].requiredLength} caracteres`);
+              } else if (keyError === 'maxlength') {
+                this.formErrors.push(`${key} no debe exceder ${controlErrors[keyError].requiredLength} caracteres`);
+              } else if (keyError === 'pattern') {
+                this.formErrors.push(`${key} tiene un formato inválido`);
+              }
+            });
+          }
+        }
+      });
+
+      console.log('Errores del formulario:', this.formErrors);
+      console.log('Errores detallados del formulario:', detailedErrors);
+
+      return; // Sale de la función porque el formulario es inválido
+    } else {
+      console.log('Formulario válido', this.registerForm.value);
+    }
+  }
+
+  onButtonClick(): void {
+    console.log('Método onButtonClick llamado');
+    if (this.registerForm.invalid) {
+      this.formErrors = [];
+      Object.keys(this.registerForm.controls).forEach(key => {
+        const controlErrors = this.registerForm.get(key)?.errors;
+        if (controlErrors) {
+          Object.keys(controlErrors).forEach(keyError => {
+            if (keyError === 'required') {
+              this.formErrors.push(`${key} es obligatorio22222`);
+            } else if (keyError === 'minlength') {
+              this.formErrors.push(`${key} debe tener al menos ${controlErrors[keyError].requiredLength} caracteres222`);
+            } else if (keyError === 'maxlength') {
+              this.formErrors.push(`${key} no debe exceder ${controlErrors[keyError].requiredLength} caracteres2222`);
+            } else if (keyError === 'pattern') {
+              this.formErrors.push(`${key} tiene un formato inválido22222`);
+            }
+          });
+        }
+      });
+
+      console.log('Errores del formulario:', this.formErrors);
+    }
+  }
 }
---
+++ src/app/shared/ui/input/input.component.ts
@@ -0,0 +1,119 @@
+import { Component, Input, Output, EventEmitter, OnInit, forwardRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, FormControl, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators } from '@angular/forms';
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, NgIf],
+  templateUrl: './input.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() id: string = '';
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() value: string = '';
+  @Input() label: string = '';
+  @Input() required: boolean = false;
+  @Input() minlength?: number;
+  @Input() maxlength?: number;
+  @Input() pattern?: string;
+  @Input() min?: number | string;
+  @Input() max?: number | string;
+  @Input() enablePasswordValidation: boolean = false;
+  @Output() valueChange = new EventEmitter<string>();
+
+  inputControl!: FormControl;
+  inputClass: string = 'block w-full py-2 text-textMuted bg-white border rounded-lg px-4 focus:border-primary focus:ring-primary focus:outline-none focus:ring focus:ring-opacity-40';
+  showError: boolean = false;
+  errorMessage: string = '';
+
+  private onChange = (value: any) => {};
+  private onTouched = () => {};
+
+  ngOnInit(): void {
+    const validators = this.buildValidators();
+    this.inputControl = new FormControl(this.value, validators);
+
+    this.inputControl.valueChanges.subscribe(value => {
+      this.onChange(value);
+      this.valueChange.emit(value);
+      this.checkValidation();
+    });
+  }
+
+  buildValidators() {
+    const validators = [];
+    if (this.required) {
+      validators.push(Validators.required);
+    }
+    if (this.minlength !== undefined) {
+      validators.push(Validators.minLength(this.minlength));
+    }
+    if (this.maxlength !== undefined) {
+      validators.push(Validators.maxLength(this.maxlength));
+    }
+    if (this.pattern) {
+      validators.push(Validators.pattern(this.pattern));
+    }
+    if (this.min !== undefined) {
+      validators.push(Validators.min(Number(this.min)));
+    }
+    if (this.max !== undefined) {
+      validators.push(Validators.max(Number(this.max)));
+    }
+    return validators;
+  }
+
+  checkValidation(): void {
+    const errors = this.inputControl.errors;
+    this.showError = !!errors;
+
+    if (errors) {
+      if (errors['required']) {
+        this.errorMessage = 'Este campo es obligatorio';
+      } else if (errors['minlength']) {
+        this.errorMessage = `Debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+      } else if (errors['maxlength']) {
+        this.errorMessage = `No debe exceder ${errors['maxlength'].requiredLength} caracteres`;
+      } else if (errors['pattern']) {
+        this.errorMessage = 'Formato inválido';
+      } else if (errors['min']) {
+        this.errorMessage = `Debe ser mayor o igual a ${errors['min'].min}`;
+      } else if (errors['max']) {
+        this.errorMessage = `Debe ser menor o igual a ${errors['max'].max}`;
+      } else {
+        this.errorMessage = 'Entrada inválida';
+      }
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
+  // Métodos necesarios para ControlValueAccessor
+  writeValue(value: any): void {
+    if (value !== undefined) {
+      this.inputControl.setValue(value);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.inputControl.disable() : this.inputControl.enable();
+  }
+
+}
