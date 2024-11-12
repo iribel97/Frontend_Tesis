9a964766ca4106a490c4adb34654a21abf12e7a5
Erick Montes <erickmontes77.jm@gmail.com>
2024-11-12 12:58:05 -0500

    Elimina AuthComponent y sus archivos relacionados.
    Se eliminó el componente `AuthComponent` junto con sus archivos CSS, HTML, spec.ts y .ts. Se realizaron ajustes de estilo en otros componentes y se incorporaron nuevos colores en la configuración de Tailwind. Ahora se utiliza `TemplateAuthComponent` en las páginas de login y registro.

--- src/app/pages/page-login/page-login.component.ts
+++ src/app/pages/page-login/page-login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FormRegisterComponent} from "../../forms/form-register/form-register.component";
+import {TemplateAuthComponent} from "../../templates/template-auth/template-auth.component";
 
 @Component({
   selector: 'app-page-login',
   standalone: true,
-  imports: [],
+    imports: [
+        FormRegisterComponent,
+        TemplateAuthComponent
+    ],
   templateUrl: './page-login.component.html',
   styleUrl: './page-login.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
       fontFamily: {
         handwriting: ['Pacifico', 'cursive'],
       },
+      colors: {
+        primary: '#4D869C',
+        'primary-hover': '#3A6B7E',
+        secondary: '#F0B7A9',
+        'secondary-hover': '#D89882',
+        terciary: '#7AB2B2',
+        'terciary-hover': '#639999',
+        quaternary: '#cde8e5',
+        'quaternary-hover': '#AACFCB',
+        quinary: '#EEF7FF',
+        'quinary-hover': '#D4E7FF',
+        darkGray: '#333333',
+        'darkGray-700': '#555555',
+        'darkGray-500': '#777777',
+        textMuted: '#4B5563', // Equivalente a gray-700
+        mainBackground: '#F9FAFB', // Similar a gray-50
+      },
     },
   },
   plugins: [],
