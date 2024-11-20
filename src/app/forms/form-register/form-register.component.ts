961e5379141860a807337d95fe71dba074df2d1a
darkod <erickmontes77.jm@gmail.com>
2024-11-19 23:03:54 -0500

    mejora de muestra de validaciones
    Se mejoro la forma en la cual se muestran los errores, se añadio que se
    muestren los campos que estan incorrectos y no solo los errores y
    tambien se creo una variable para manejar cuando se vean y cuando no la
    informacion de las validaciones.

--- src/app/forms/form-register/form-register.component.ts
+++ src/app/forms/form-register/form-register.component.ts
@@ -1,64 +1,63 @@
-import {Component, OnInit, ChangeDetectorRef, NgZone} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {NgForOf, NgIf} from "@angular/common";
-import {InputComponent} from "../../shared/ui/input/input.component";
+import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgForOf, NgIf } from "@angular/common";
+import { InputComponent } from "../../shared/ui/input/input.component";
 
 @Component({
-    selector: 'form-register',
-    standalone: true,
-    imports: [ReactiveFormsModule, InputComponent, NgIf, NgForOf],
-    templateUrl: './form-register.component.html',
+  selector: 'form-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputComponent, NgIf, NgForOf],
+  templateUrl: './form-register.component.html',
 })
 export class FormRegisterComponent implements OnInit {
-    formErrors: { [key: string]: string } = {};
-    formRegister!: FormGroup;
+  formErrors: { [key: string]: string } = {};
+  formRegister!: FormGroup;
+  sendform = false;
 
-    constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef, private ngZone: NgZone) {
-    }
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef, private ngZone: NgZone) {
+  }
 
-    ngOnInit(): void {
-        this.formRegister = this.fb.group({
-            ci: [''],
-            name: [''],
-            last_name: [''],
-            email: [''],
-            password: [''],
-            phone: [''],
-            address: [''],
-            birth_date: [''],
-            gender: [''],
-            company: [''],
-            company_phone: [''],
-            company_address: [''],
-        });
-        this.formRegister.valueChanges.subscribe(() => {
-            this.ngZone.run(() => {
-                this.cdr.detectChanges();
-            });
-        });
-    }
+  ngOnInit(): void {
+    this.formRegister = this.fb.group({
+      ci: [''],
+      name: [''],
+      last_name: [''],
+      email: [''],
+      password: [''],
+      phone: [''],
+      address: [''],
+      birth_date: [''],
+      gender: [''],
+      company: [''],
+      company_phone: [''],
+      company_address: [''],
+    });
+    this.formRegister.valueChanges.subscribe(() => {
+      this.ngZone.run(() => {
+        this.cdr.detectChanges();
+      });
+    });
+  }
 
-    onSubmit() {
-        if (Object.keys(this.formErrors).length > 0) {
-            console.log('Form has errors:', this.formErrors);
-        } else {
-            console.log('Form submitted successfully');
-        }
+  onSubmit() {
+    this.sendform = true;
+    if (Object.keys(this.formErrors).length > 0) {
+      console.log('Form has errors:', this.formErrors);
+    } else {
+      console.log('Form submitted successfully');
     }
+  }
 
-    onValueChange(value: string) {
+  onErrorsChange(event: { id: string, errorMessage: string }) {
+    if (event.errorMessage) {
+      this.formErrors[event.id] = event.errorMessage;
+    } else {
+      delete this.formErrors[event.id];
     }
+    this.cdr.detectChanges(); // Trigger change detection manually
+  }
 
-    onErrorsChange(event: { id: string, errorMessage: string }) {
-        if (event.errorMessage) {
-            this.formErrors[event.id] = event.errorMessage;
-        } else {
-            delete this.formErrors[event.id];
-        }
-        this.cdr.detectChanges(); // Trigger change detection manually
-    }
-
-    getFormErrorsKeys(): string[] {
-        return Object.keys(this.formErrors);
-    }
+  getFormErrorsKeys(): string[] {
+    return Object.keys(this.formErrors);
+  }
 }
