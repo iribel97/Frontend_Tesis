be9d5682b6184a210adb3e19b7222b7f7f8ee47e
Erick Montes <erickmontes77.jm@gmail.com>
2024-11-20 14:06:30 -0500

    Añade gráficos y función de clic fuera al dropdown
    Se incorporaron gráficos de barras, líneas y pastel utilizando Chart.js y ng2-charts en home.component. Se implementó la directiva clickOutside y se mejoró la funcionalidad del dropdown para cerrarse al clicar fuera en principal.component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+// Registrar todos los componentes necesarios de Chart.js
+Chart.register(...registerables);
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        BaseChartDirective
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent {
+// Datos y configuración para el gráfico de barras
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartData: ChartConfiguration['data'] = {
+    labels: this.barChartLabels,
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ]
+  };
 
+  // Datos y configuración para el gráfico de línea
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public lineChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartData: ChartConfiguration['data'] = {
+    labels: this.lineChartLabels,
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        fill: false,
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        fill: false,
+      }
+    ]
+  };
+
+  // Datos y configuración para el gráfico de pastel
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public pieChartData: ChartConfiguration['data'] = {
+    labels: this.pieChartLabels,
+    datasets: [
+      {
+        data: [300, 500, 100]
+      }
+    ]
+  };
 }
--- src/app/templates/principal/principal.component.ts
+++ src/app/templates/principal/principal.component.ts
@@ -1,25 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {BreadcrumbService} from "../../services/breadcrumb.service";
+import {NgClass} from "@angular/common";
+import {ClickOutsideDirective} from "../../shared/directivas/click-outside.directive";
 
 @Component({
-  selector: 'app-principal',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+    selector: 'app-principal',
+    standalone: true,
+    imports: [RouterOutlet, NgClass, ClickOutsideDirective],
+    templateUrl: './principal.component.html',
+    styleUrl: './principal.component.css'
 })
-export class PrincipalComponent implements OnInit{
-  constructor(
-                private breadcrumbService: BreadcrumbService){
-
+export class PrincipalComponent implements OnInit {
+    constructor(
+        private breadcrumbService: BreadcrumbService) {
     }
 
-      breadcrumb: string[] = [];
+    breadcrumb: string[] = [];
+    isDropdownOpen = false;
 
-  ngOnInit() {
-    this.breadcrumbService.breadcrumb$.subscribe(breadcrumb => {
+    ngOnInit() {
+        this.breadcrumbService.breadcrumb$.subscribe(breadcrumb => {
             this.breadcrumb = breadcrumb;
         });
+    }
+
+    toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown(event: Event) {
+    if (!(event.target as HTMLElement).closest('#dropdown-account')) {
+      this.isDropdownOpen = false;
+    }
   }
 }
